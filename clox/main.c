192c7c23f447a72dfe08182fcf3c1dcd0a0980bb
zeroli <zeroliql@gmail.com>
2023-01-04 14:51:48 -0800

    add arithmetic calculation +/-/*// support

--- clox/main.c
+++ clox/main.c
@@ -11,10 +11,24 @@ int main(int argc, const char* argv[])
     initChunk(&chunk);
 
     // write op `load constant value` to byte code
+    // expression: -((1.2 + 3.4) / 5.6)
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
     writeChunk(&chunk, OP_NEGATE, 123);
+
     // write op `return` to byte code
     writeChunk(&chunk, OP_RETURN, 123);
 
