9014b7ebbb5533fc3cb1d0963befbb20b4d6c908
zeroli <zeroliql@gmail.com>
2023-01-04 14:31:21 -0800

    add negate op support

--- clox/vm.c
+++ clox/vm.c
@@ -49,6 +49,10 @@ InterpretResult run()
             push(constant);
             break;
         }
+        case OP_NEGATE: {
+            push(-pop());
+            break;
+        }
         case OP_RETURN: {
             printValue(pop());
             printf("\n");
