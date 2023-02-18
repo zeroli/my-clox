ae78a8b16f61aa821c76739bfdeb625b72f61b23
zero.li <zeroliql@gmail.com>
2023-02-18 15:03:34 -0800

    clox: not allow this being used outside class

--- clox/compiler.c
+++ clox/compiler.c
@@ -69,8 +69,13 @@ typedef struct Compiler {
     int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+    struct ClassCompiler* enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler* current = NULL;
+ClassCompiler* currentClass = NULL;
 
 static Chunk* currentChunk() {
     return &current->function->chunk;
@@ -483,6 +488,10 @@ static void variable(bool canAssign) {
 }
 
 static void this_(bool canAssign) {
+    if (currentClass == NULL) {
+        error("Can't use 'this' outside of a class.");
+        return;
+    }
     // `this=` not allowed, so pass `false` here
     variable(false);
 }
@@ -561,6 +570,11 @@ static void classDeclaration() {
 
     emitBytes(OP_CLASS, nameConstant);
     defineVariable(nameConstant);
+
+    ClassCompiler classCompiler;
+    classCompiler.enclosing = currentClass;
+    currentClass = &classCompiler;
+
     // push class varaiable back to top of stack at runtime
     namedVariable(className, false);
 
@@ -570,6 +584,8 @@ static void classDeclaration() {
     }
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+
+    currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {
